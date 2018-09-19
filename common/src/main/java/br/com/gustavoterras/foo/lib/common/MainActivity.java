d72a1ed8ec997ba5f61d4983153062364703a288
gustavoterras <gtsfa29101988>
2018-09-18 23:26:20 -0300

    first commit.

---
+++ common/src/main/java/br/com/gustavoterras/foo/lib/common/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.gustavoterras.foo.lib.common;
+
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.activity_main);
+    }
+}
